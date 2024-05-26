afe3bfd05fa1a8dd45d068e7e26298b21c81574e
yash-kh <user2@Yashs-MacBook-Air.local>
2024-05-26 15:31:01 +0530

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,36 @@
-import { Socket } from "socket.io";
-import http from "http";
+import express from 'express';
+import http from 'http';
+import { Server, Socket } from 'socket.io';
+import { UserManager } from './managers/UserManger';
 
-import { Server } from 'socket.io';
-import { UserManager } from "./managers/UserManger";
+// Create an instance of Express
+const app = express();
 
-const server = http.createServer(http);
+// Create an HTTP server using the Express app
+const server = http.createServer(app);
 
+// Create a new instance of Socket.IO server with the HTTP server
 const io = new Server(server, {
   cors: {
     origin: "*"
   }
 });
 
+// Create an instance of UserManager
 const userManager = new UserManager();
 
+// Set up a connection event for Socket.IO
 io.on('connection', (socket: Socket) => {
   console.log('a user connected');
   userManager.addUser("randomName", socket);
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
     userManager.removeUser(socket.id);
-  })
+  });
 });
 
-server.listen(3000, () => {
-    console.log('listening on *:3000');
+// Set the Express app to listen on port 3000
+server.listen(4000, () => {
+  console.log('listening on *:4000');
 });
